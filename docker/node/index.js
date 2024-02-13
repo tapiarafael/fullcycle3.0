f59063a8a8463af15722ebb5ab7fb62d89034cd7
Rafael Tapia <rafaelsilvatapia@gmail.com>
2024-02-13 13:40:55 -0300

    docker-compose files

--- docker/node/index.js
+++ docker/node/index.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const mysql = require("mysql");
+const config = {
+  host: "db",
+  port: "3306",
+  user: "root",
+  password: "root",
+  database: "nodedb",
+};
+
+const connection = mysql.createConnection(config);
+
+const sql = `INSERT INTO people(name) VALUES('Rafael')`;
+connection.query(sql);
+connection.end();
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
